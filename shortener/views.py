04bd10598397074a76ee7bca9991621ff5d684d3
Muhammad Shiraz <shirazshahzad2003@gmail.com>
2025-07-16 20:52:00 +0500

    create retrieve endpoint  in views

--- shortener/views.py
+++ shortener/views.py
@@ -11,3 +11,14 @@ def create_short_url(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def retrieve_original_url(request, code):
+    try:
+        url = ShortURL.objects.get(short_code=code)
+        url.access_count += 1
+        url.save()
+        return Response(ShortURLSerializer(url).data)
+    except ShortURL.DoesNotExist:
+        return Response({"error": "Short URL not found"}, status=404)
+
